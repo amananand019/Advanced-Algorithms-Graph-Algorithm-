1c864ec17ed62346d72560d8b3abac9f14c7f0a9
Aman Anand <aman019anand@gmail.com>
2020-12-18 01:05:09 +0530

    Bug Fix BellmanFord

--- src/shortestPathAlgorithms/BellmanFordAlgorithm/App.java
+++ src/shortestPathAlgorithms/BellmanFordAlgorithm/App.java
@@ -12,7 +12,7 @@ public class App {
         vertexList.add(new Vertex("C"));
         vertexList.add(new Vertex("D"));
         vertexList.add(new Vertex("E"));
-
+//
         List<Edge> edgeList = new ArrayList<>();
         edgeList.add(new Edge(vertexList.get(0),vertexList.get(1),5));
         edgeList.add(new Edge(vertexList.get(0),vertexList.get(3),-4));
@@ -24,7 +24,16 @@ public class App {
         edgeList.add(new Edge(vertexList.get(4),vertexList.get(0),6));
         edgeList.add(new Edge(vertexList.get(4),vertexList.get(2),7));
 
+//        edgeList.add(new Edge(vertexList.get(0), vertexList.get(1),1));
+//        edgeList.add(new Edge(vertexList.get(0), vertexList.get(2),-1));
+//        edgeList.add(new Edge(vertexList.get(1), vertexList.get(2),1));
+
         BellmanFord bellmannFordAlgorithm = new BellmanFord(vertexList,edgeList);
-        bellmannFordAlgorithm.bellmanFord(vertexList.get(4),vertexList.get(3));
+        List<Vertex> vertexList1= bellmannFordAlgorithm.bellmanFord(vertexList.get(4),vertexList.get(3));
+
+        for(Vertex v:vertexList1){
+            System.out.print(v+" ");
+        }
+
     }
 }
--- src/shortestPathAlgorithms/BellmanFordAlgorithm/BellmanFord.java
+++ src/shortestPathAlgorithms/BellmanFordAlgorithm/BellmanFord.java
@@ -1,5 +1,7 @@
 package shortestPathAlgorithms.BellmanFordAlgorithm;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BellmanFord {
@@ -7,23 +9,25 @@ public class BellmanFord {
     private List<Edge> edgeList;
     private List<Vertex> vertexList;
 
+    private List<Vertex> returnList;
+
     public BellmanFord(List<Vertex> vertexList, List<Edge> edgeList) {
         this.edgeList = edgeList;
         this.vertexList = vertexList;
+        this.returnList = new ArrayList<>();
     }
 
-    public void bellmanFord(Vertex sourceVertex, Vertex targetVertex) {
+    public List<Vertex> bellmanFord(Vertex sourceVertex, Vertex targetVertex) {
 
         sourceVertex.setDistance(0);
 
-        for (Vertex vertex : vertexList) {
+        for (int i = 0; i < vertexList.size() - 1; ++i) {
             for (Edge edge : edgeList) {
-
-                if ( edge.getStartVertex().getDistance() == Integer.MAX_VALUE ) {
+                if (edge.getStartVertex().getDistance() == Double.MAX_VALUE) {
                     continue;
                 }
 
-                int newDistance = (int) (edge.getStartVertex().getDistance() + edge.getWeight());
+                double newDistance = edge.getStartVertex().getDistance() + edge.getWeight();
 
                 if (newDistance < edge.getTargetVertex().getDistance()) {
                     edge.getTargetVertex().setDistance(newDistance);
@@ -33,29 +37,34 @@ public class BellmanFord {
         }
 
         for (Edge edge : edgeList) {
-            if (edge.getStartVertex().getDistance() != Integer.MAX_VALUE) {
-                if ( hasCycle(edge) ) {
+            if (edge.getStartVertex().getDistance() != Double.MAX_VALUE) {
+                if (hasCycle(edge)) {
                     System.out.println("Negative edge weight cycles detected!");
-                    return;
+                    return null;
                 }
             }
         }
 
-        if (targetVertex.getDistance() != Integer.MAX_VALUE) {
+        if (targetVertex.getDistance() != Double.MAX_VALUE) {
             System.out.println("There is a shortest path from sourco to target, with cost: " + targetVertex.getDistance());
 
-            Vertex actualVErtex = targetVertex;
-            while( actualVErtex.getPreviousVertex() != null ){
-                System.out.print(actualVErtex+"-");
-                actualVErtex=actualVErtex.getPreviousVertex();
+            Vertex actualVertex = targetVertex;
+            while (actualVertex.getPreviousVertex() != null) {
+                returnList.add(actualVertex);
+                actualVertex = actualVertex.getPreviousVertex();
             }
+
+            if (actualVertex == sourceVertex)
+                returnList.add(actualVertex);
 
         } else {
             System.out.println("There is no path from source to target...");
         }
+        Collections.reverse(returnList);
+        return returnList;
     }
 
-    private boolean hasCycle(Edge edge){
+    private boolean hasCycle(Edge edge) {
         return edge.getTargetVertex().getDistance() > edge.getStartVertex().getDistance() + edge.getWeight();
     }
 }
